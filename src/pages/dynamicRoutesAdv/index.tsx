517cf8f17fe78294bdfd4b9f750ce4fe94a3fbd2
juhonamnam <namjhnam@gmail.com>
2022-04-26 17:11:04 +0900

    edit description

--- src/pages/dynamicRoutesAdv/index.tsx
+++ src/pages/dynamicRoutesAdv/index.tsx
@@ -7,8 +7,11 @@ const DynamicRoutesHome = ({ todoList }: { todoList: ITodoItem[] }) => {
     <div className="container">
       <h1>Dynamic Routes Advanced</h1>
       <p>
-        To-Do List 목록에서 id 번호를 path variable로 받아서 각각의 아이템을
-        개별조회하는 상황을 가정합니다.
+        path variable과 외부 API 요청이 둘 다 발생하는 경우 상황은 더
+        복잡해집니다.
+        <br />
+        아래의 예시는 To-Do List 목록에서 id 번호를 path variable로 받아 새로운
+        페이지에 각각의 아이템을 개별조회하는 상황을 가정합니다.
       </p>
       <ul>
         <li>
@@ -41,8 +44,8 @@ const DynamicRoutesHome = ({ todoList }: { todoList: ITodoItem[] }) => {
           Incremental Static Regeneration (ISR) 으로 To-Do 아이템 개별조회하기
           <TodoItemId todoList={todoList} endpoint="dynamicRoutesAdv/ISR" />
           <p>
-            SSG 방식으로 미리 빌드를 하되 일정 주기를 두어 새로 생겨나거나
-            삭제되는 아이템에 대해 업데이트합니다.
+            SSG 방식으로 미리 빌드를 하되 일정 주기를 두어 주가기 끝난 이후에
+            들어오는 페이지 요청에 대해서 새롭게 빌드합니다.
           </p>
         </li>
       </ul>
