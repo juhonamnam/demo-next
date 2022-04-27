c1bfc7f592beb77f28eb46636358ce4b5312d4f0
juhonamnam <namjhnam@gmail.com>
2022-04-27 11:55:01 +0900

    modify image optimization

--- src/pages/imageOptimization/index.tsx
+++ src/pages/imageOptimization/index.tsx
@@ -8,7 +8,7 @@ const ImageDemo = () => {
         <p>
           Next.js 에 내장되어있는{' '}
           <code>
-            {'<'}Image {'/>'}
+            {'<'}Image{'>'}
           </code>{' '}
           컴포넌트는 기존 이미지를 압축시켜서 데이터 전달양을 최소화 합니다.
           이미지 데이터 양은 브라우저 개발자 도구의 네트워크 탭에서 확인할 수
@@ -18,42 +18,44 @@ const ImageDemo = () => {
         <ul>
           <li>
             <code>
-              {'<'}img {'/>'}
+              {'<'}img{'>'}
             </code>
             <br />
             <img src="/test_image.jpg" width={480} height={270} alt="" />
           </li>
           <li>
             <code>
-              {'<'}Image {'/>'}
+              {'<'}Image{'>'}
             </code>
             <br />
+            {/* <Image>는 width와 height가 필수값입니다. */}
             <Image src="/test_image.jpg" width={480} height={270} alt="" />
           </li>
         </ul>
         <p>
           <code>
-            {'<'}Image {'/>'}
+            {'<'}Image{'>'}
           </code>{' '}
-          태그는 lazy loading을 기본으로 탑재하여 화면에 표시되지 않는 이미지를
-          미리 불러오지 않음으로 첫 페이지 로딩시간을 줄여줍니다. 아래로
-          스크롤하여 lazy loading 기능을 확인하세요.
+          는 lazy loading을 기본으로 탑재하여 화면에 표시되지 않는 이미지를 미리
+          불러오지 않음으로 첫 페이지 로딩시간을 줄여줍니다. 아래로 스크롤하여
+          lazy loading 기능을 확인하세요.
         </p>
       </div>
 
       <ul>
         <li>
           <code>
-            {'<'}img {'/>'}
+            {'<'}img{'>'}
           </code>
           <br />
           <img src="/test_image.jpg" width={480} height={270} alt="" />
         </li>
         <li>
           <code>
-            {'<'}Image {'/>'}
+            {'<'}Image{'>'}
           </code>
           <br />
+          {/* <Image>는 width와 height가 필수값입니다. */}
           <Image src="/test_image.jpg" width={480} height={270} alt="" />
         </li>
       </ul>
